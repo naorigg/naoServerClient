6600422da11d33ad7770b55f5e80b397548ad3e6
Nathanaël Schuhler <shikugen.chi@gmail.com>
2022-07-09 10:45:28 +0100

    Logger Error Fixed ! Continue afterwards with LoggerName

--- src/main/java/jp/naori/client/Main.java
+++ src/main/java/jp/naori/client/Main.java
@@ -1,9 +1,10 @@
 package jp.naori.client;
 // I directly used "Java Logger"
-import java.util.logging.Logger;
+import java.util.logging.*;
 public class Main {
     public static Logger Logger;
     public static void main(String[] args) {
-
+        // See what AriLogger does and find how to do it with 'Java Logger'
+        // Maybe with : https://www.geeksforgeeks.org/logger-getname-method-in-java-with-examples/
     }
 }
